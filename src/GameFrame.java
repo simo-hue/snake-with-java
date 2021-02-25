d201f5e872e137d8082104fc7ae9d62a9d48daa5
MatioliSimone <simone.mattioli@marconirovereto.it>
2021-02-25 17:50:53 +0100

    Update GameFrame.java

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,12 +1,16 @@
-
 import javax.swing.JFrame;
-
 
 public class GameFrame extends JFrame{
 
-    public GameFrame() {
-        this.add(new GamePanel());
-    }
-
-    
+	GameFrame(){
+			
+		this.add(new GamePanel());
+		this.setTitle("Gioco Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+	}
 }
