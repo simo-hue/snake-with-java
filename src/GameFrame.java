3bab2eaca5924784cfe8d815e09e860020fb120b
MatioliSimone <simone.mattioli@marconirovereto.it>
2021-02-25 17:49:10 +0100

    Update GameFrame.java
    errore risolto pt2

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,15 +1,12 @@
-import javax.swing.JFrame;
-public class GameFrame {
 
-    GameFrame(){
-			
-		this.add(new GamePanel());
-		this.setTitle("Snake");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setResizable(false);
-		this.pack();
-		this.setVisible(true);
-		this.setLocationRelativeTo(null);
-		
-	}
+import javax.swing.JFrame;
+
+
+public class GameFrame extends JFrame{
+
+    public GameFrame() {
+        this.add(new GamePanel());
+    }
+
+    
 }
